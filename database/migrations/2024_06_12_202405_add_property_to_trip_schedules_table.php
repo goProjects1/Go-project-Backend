3fae98c4a2006b362741d54dc2eed33f2d7c9a43
lumsys <sunday4oged@yahoo.com>
2024-06-12 22:22:39 +0100

    newDataSources

---
+++ database/migrations/2024_06_12_202405_add_property_to_trip_schedules_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPropertyToTripSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trip_schedules', function (Blueprint $table) {
+            //
+            $table->foreignId('ownProperty')->nullable();
+            $table->string('destLatitude')->nullable();
+            $table->string('destLongitude')->nullable();
+        });
+    }
+
+
+}
