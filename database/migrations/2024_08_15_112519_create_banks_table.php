01fe53f92e74aa55f3f053c157d28a943ef2d122
lumsys <sunday4oged@yahoo.com>
2024-08-15 12:52:42 +0100

    transactions

---
+++ database/migrations/2024_08_15_112519_create_banks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->double('account_number')->nullable();
+            $table->double('account_name')->nullable();
+            $table->string('referenceId')->nullable();
+            $table->string('bankName')->nullable();
+            $table->string('bankCode')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banks');
+    }
+}
